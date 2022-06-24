70a6141b9cc19433c367b5875fd61bad334d3c6b
yujinkim11 <wowyj26@naver.com>
2022-06-24 11:36:10 +0900

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { mainStyle } from "../styles/globalStyle";
+
+const SHeader = styled.div`
+  width: 100%;
+  max-width: 100%;
+  height: 80px;
+  padding: 0 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+
+const Logo = styled.h3`
+  font-size: 28px;
+  font-weight: 800;
+  a {
+    color: ${mainStyle.mainColor};
+  }
+`;
+
+const MenuWrap = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  a {
+    color: ${mainStyle.color};
+  }
+`;
+
+const Menu = styled.li`
+  margin-left: 100px;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const Header = () => {
+  return (
+    <SHeader>
+      <Logo>
+        <Link to={"/"}>YJMOVIE</Link>
+      </Logo>
+
+      <MenuWrap>
+        <Menu>
+          <Link to={"/"}>Home</Link>
+        </Menu>
+        <Menu>
+          <Link to={"/Search"}>Search</Link>
+        </Menu>
+      </MenuWrap>
+    </SHeader>
+  );
+};
