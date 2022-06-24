8c4c7c6dcee9b1085176717a0bf71d1b079f2ff6
yujinkim11 <wowyj26@naver.com>
2022-06-24 12:55:15 +0900

    api

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "a0c00eb5e3cae224bdc5208275b7c9b8",
+    language: "ko-kr",
+  },
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  topRated: () => api.get("movie/top_rated"),
+  latest: () => api.get("movie/latest"),
+  upComming: () => api.get("movie/upcoming"),
+};
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+import { movieApi } from "../../../api";
+// console.log(movieApi.nowPlaying());
+
 export const Home = () => {
+  useEffect(() => {
+    const movieData = async () => {
+      const {
+        data: { results },
+      } = await movieApi.nowPlaying();
+      console.log(results);
+      // console.log(now);
+    };
+    movieData();
+  }, []);
+
   return <div>Home</div>;
 };
